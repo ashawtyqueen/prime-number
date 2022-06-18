9142f052bee148ae160f03c49ada44ffc4248b34
Tokede Anuoluwapo <tokede29@gmail.com>
2022-06-18 12:40:57 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a program to check if a is prime
+
+#the function
+def is_prime(num):
+
+#the conditional statement
+    if num % 2 == 0:
+        return False
+    else:
+        return True
+
+print(is_prime(0))
